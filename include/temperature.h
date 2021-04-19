1889e59dbb36fff235786832df47d495f2255927
lass6230 <due6230@gmail.com>
2021-04-19 13:43:18 +0200

    temp class

---
+++ include/temperature.h
@@ -0,0 +1,20 @@
+#pragma once
+
+class temperature
+{
+private:
+    double temp;
+public:
+    temperature();
+    ~temperature();
+    double getTemperature(){return temp;};
+
+};
+
+temperature::temperature(/* args */)
+{
+}
+
+temperature::~temperature()
+{
+}
