07e6b8482e9275f927b5b0b9d02969a26a093240
lass6230 <due6230@gmail.com>
2021-04-22 08:50:11 +0200

    hardstate

--- include/hardstate_output.h
+++ include/hardstate_output.h
@@ -3,14 +3,16 @@
 class HardstateOutput
 {
 private:
-    /* data */
+    double fan_speed;
+    double watering_speed;
+    double light_intensity;
 public:
     HardstateOutput(/* args */);
     ~HardstateOutput();
 
-    double SetFanSpeed();
-    double SetWateringSpeed();
-    double SetLightIntensity();
+    void SetFanSpeed(double fan_speed_1);
+    void SetWateringSpeed(double set_watering_speed);
+    void SetLightIntensity(double set_light_intensity);
 };
 
 
