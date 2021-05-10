dad73c94dab823b8c05290528c2caaee53c048fd
lass6230 <due6230@gmail.com>
2021-05-10 21:43:14 +0200

    climate control

--- include/climate_control.h
+++ include/climate_control.h
@@ -1,19 +1,25 @@
 #pragma once
 #include <plant.h>
 
-class CimateControl
+
+
+class ClimateControl
 {
 private:
     double target_temperature;
+    
+
 public:
-    CimateControl(/* args */);
-    CimateControl(double plant_target_temperature){
+    ClimateControl(/* args */);
+    ClimateControl(double plant_target_temperature){
         target_temperature = plant_target_temperature;
     }
-     CimateControl(Plant& plant){
+     ClimateControl(Plant& plant){
         target_temperature = plant.GetTargetTemperature();
+        
+
     }
-    ~CimateControl();
+    ~ClimateControl();
     void controlclimate();
 };
 
