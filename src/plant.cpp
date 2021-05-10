fbed210af9888e6640facc95bea3c37483856a8a
lass6230 <due6230@gmail.com>
2021-05-10 16:38:54 +0200

    a

--- src/plant.cpp
+++ src/plant.cpp
@@ -4,12 +4,8 @@
 
 void Plant::SetPlantType(){
     std::cin >> plant_type;
-<<<<<<< HEAD
     
 
-=======
-    std::cout <<"Hello world\n";
->>>>>>> 2900d1391c6ac40abf66addcbb59707349f8d043
 }
 
 void Plant::GetCurrentPlantInfo(){
