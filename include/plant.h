e393e7300c90b483347fdfcb83920f87e0caec7d
lass6230 <due6230@gmail.com>
2021-05-10 14:26:01 +0200

    plant.h

--- include/plant.h
+++ include/plant.h
@@ -1,1 +1,22 @@
 #pragma once
+#include <string>
+
+class Plant
+{
+private:
+    double grow_rate;
+    std::string plant_type;
+public:
+    Plant(/* args */);
+    ~Plant();
+    void GetCurrentPlantInfo();
+
+};
+
+Plant::Plant(/* args */)
+{
+}
+
+Plant::~Plant()
+{
+}
