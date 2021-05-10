2d6208cc52953f1768f7ea3c719452dfbb3ea579
lass6230 <due6230@gmail.com>
2021-05-10 22:04:04 +0200

    cleanup

--- include/plant_lib.h
+++ include/plant_lib.h
@@ -4,18 +4,12 @@ class PlantLib
 {
 private:
     double plant_info[4][2] = {{1,1},{2,2},{3,3},{4,4}};
-    double tomato_info[2] = {2,2};
-    double cucumber_info[2] = {3,3};
-    double paradise_tree_info[2] = {4,4};
-    double cactus_info[2] = {1,1};
+    
 
 public:
     PlantLib(/* args */);
     ~PlantLib();
-    double GetCactusInfo();
-    double GetTomatoInfo();
-    double GetCucumberInfo();
-    double GetParadiseInfo();
+    
     void GetPlantInfo(int plant_number, double &grow_rate, double &target_temperature);
 };
 
